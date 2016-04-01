865f4dd4972527f7bdbfe183d55687ba9c527ef5
Agapov Alex <agapov.one@gmail.com>
2016-04-02 02:39:22 +0500

    Initial Commit

---
+++ HandbookTests/HandbookTests.swift
@@ -0,0 +1,36 @@
+//
+//  HandbookTests.swift
+//  HandbookTests
+//
+//  Created by Алексей Агапов on 02/04/16.
+//  Copyright © 2016 ru.urfu. All rights reserved.
+//
+
+import XCTest
+@testable import Handbook
+
+class HandbookTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
